b5cb94ca157ba517000c3c7b7e2e107e598a15aa
Brian Gardner <gardner.brian@gmail.com>
2020-04-18 12:25:04 -0500

    add basic performer sign-up react component to performer react route

---
+++ clientPerformer/Components/Signup/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return (
+      <div>
+      Signup
+      </div>
+    )
+  }
+}
+
+export default Signup;
--- clientPerformer/index.js
+++ clientPerformer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from './Components/App.js';
+import Signup from './Components/Signup/Signup.jsx'
 
 ReactDOM.render(<BrowserRouter basename="/performer">
 <Switch>
@@ -9,7 +10,8 @@ ReactDOM.render(<BrowserRouter basename="/performer">
         <h1>Login</h1>
     </Route>
     <Route path="/signup">
-        <h1>Signup</h1>
+        {/* <h1>Signup</h1> */}
+        <Signup />
     </Route>
     <Route path="/">
         <App />
