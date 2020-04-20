f6c5e5f0c7b9f73555a74a8a169e04f3a0ac08bf
Brian Gardner <gardner.brian@gmail.com>
2020-04-20 15:09:31 -0500

    handle route conflict

--- clientPerformer/index.js
+++ clientPerformer/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from './Components/App.js';
-<<<<<<< HEAD
 import Signup from '../clientShared/Signup/Signup.jsx'
-=======
 import Login from '../clientShared/Login'
->>>>>>> 2eee262dda53d84f7b1473d8441db1ebba92bde8
 
 ReactDOM.render(<BrowserRouter basename="/performer">
 <Switch>
