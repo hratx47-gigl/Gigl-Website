996796fbab645d75c199ef48f66954e5c92e4d96
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-04-21 16:55:13 -0500

    "merge"

--- clientPerformer/index.js
+++ clientPerformer/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from './Components/App.js';
-<<<<<<< HEAD
 import PerformerDashboard from './Components/dashboard/PerformerDashboard';
-=======
 import Signup from '../clientShared/Signup/Signup.jsx'
->>>>>>> 21081746cecbb842ef72716b098872b346e15fea
 import Login from '../clientShared/Login'
 
 ReactDOM.render(<BrowserRouter basename="/performer">
