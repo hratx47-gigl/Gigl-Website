a22da9c35f924f3407034c59f0cfcd875e8ce7fe
Dave Hastings <dhastings725@gmail.com>
2020-04-20 20:56:10 -0400

    added render tests for login component

--- clientGig/__tests__/App.test.js
+++ clientGig/__tests__/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import {render, fireEvent, cleanup, waitFor} from '@testing-library/react'
-
+import Login from "../../clientShared/Login"
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 // this adds custom jest matchers from jest-dom
 import '@testing-library/jest-dom/extend-expect'
 import App from '../Components/App';
+
+
 
 afterEach(cleanup);
 
@@ -15,3 +18,24 @@ it('CheckboxWithLabel changes the text after click', async () => {
 
     expect(dolphin).toBeTruthy();
 });
+
+test('renders correctly', () => {
+    const {container, getByText} = render(<BrowserRouter><Login/></BrowserRouter>)
+    expect(getByText('Login to your Account')).toBeInTheDocument();
+    expect(getByText('Email address')).toBeInTheDocument();
+    expect(getByText('Password')).toBeInTheDocument();
+    expect(getByText("Don't have an Account?")).toBeInTheDocument();
+
+    expect(container).toMatchInlineSnapshot();
+  });
+
+
+
+// Test Ideas // 
+  // if the user did not put in a valid email then alert should happen
+  // if the username does not match throw alert username and/or password does not match
+  // if the password doesnt match throw alert username and/or password does not match
+
+  // check for a login button
+  
+
