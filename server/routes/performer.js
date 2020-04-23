79b1f569333a4196a5f6de19e25866d5ca63542f
Brian Gardner <gardner.brian@gmail.com>
2020-04-23 18:06:11 -0500

    add 'available' route for gigs

--- server/routes/performer.js
+++ server/routes/performer.js
@@ -19,7 +19,9 @@ router.post('/signup', [
 // router.get('/gigs', PerformerController.getPerformerGigs);
 router.get('/gigs', PerformerController.getAllGigs);
 router.get('/gigs/pending', PerformerController.getPerformerPendingGigs);
-router.post('/gigs/apply', PerformerController.postApplyToGig)
+router.get('/gigs/available', PerformerController.getPerformerAvailableGigs);
+router.post('/gigs/apply', PerformerController.postApplyToGig);
+
 
 // router.post("/signout", AuthController.postPerformerSignout);
 
